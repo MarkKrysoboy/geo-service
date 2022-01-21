e6352cb9b273ab06cba10f6f9d05d2eaf89e5f75
Mark Krysoboy <heter1209@gmail.com>
2022-01-21 15:10:24 +0300

    Split on two tests

--- src/test/java/ru/netology/sender/TestMessageSenderImpl.java
+++ src/test/java/ru/netology/sender/TestMessageSenderImpl.java
@@ -1,6 +1,7 @@
 package ru.netology.sender;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import ru.netology.entity.Country;
@@ -14,31 +15,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TestMessageSenderImpl {
-    @Test
-    void testLanguageTextFromIp() {
-        GeoService geoService = Mockito.mock(GeoServiceImpl.class);
+    GeoService geoService;
+    LocalizationService localizationService;
+    Map<String, String> headers;
+    MessageSenderImpl messageSender;
+
+    @BeforeEach
+    void startTest(){
+        geoService = Mockito.mock(GeoServiceImpl.class);
         Mockito.when(geoService.byIp(Mockito.anyString()))
                 .thenReturn(new Location("New York", Country.USA, null,  0));
         Mockito.when(geoService.byIp(Mockito.startsWith("172.")))
                 .thenReturn(new Location("Moscow", Country.RUSSIA, null, 0));
 
-        LocalizationService localizationService = Mockito.mock(LocalizationServiceImpl.class);
+        localizationService = Mockito.mock(LocalizationServiceImpl.class);
         Mockito.when(localizationService.locale(Country.RUSSIA))
                 .thenReturn("Добро пожаловать");
         Mockito.when(localizationService.locale(Country.USA))
                 .thenReturn("Welcome");
 
-        MessageSenderImpl messageSender = new MessageSenderImpl(geoService, localizationService);
-        Map<String, String> headers = new HashMap<String, String>();
-        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.44.183.149");
+        headers = new HashMap<String, String>();
+        messageSender = new MessageSenderImpl(geoService, localizationService);
+    }
 
+    @Test
+    void testLanguageTextFromIpRussia() {
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.44.183.149");
         String preferences = messageSender.send(headers);
         String expected = "Добро пожаловать";
         Assertions.assertEquals(expected, preferences);
+    }
 
+    @Test
+    void testLanguageTextFromIpEnglish() {
         headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "96.44.183.149");
-        preferences = messageSender.send(headers);
-        expected= "Welcome";
+        String preferences = messageSender.send(headers);
+        String expected= "Welcome";
         Assertions.assertEquals(expected, preferences);
     }
+
 }
